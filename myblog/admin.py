a9c8dea9a860ac0db75498ff88616c66fab7d28d
s4ngwoo <s4ngwoo.lee@gmail.com>
2023-08-09 14:56:22 +0900

    change category, series, tag fields in admin page

--- myblog/admin.py
+++ myblog/admin.py
@@ -10,21 +10,21 @@ class PostAdmin(admin.ModelAdmin):
     fields = ['title', 'content', 'author', 'category', 'series', 'tag', 'language']
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'create_date')
+    list_display = ('title', 'create_date', 'published_date')
     search_fields = ['title', 'content']
     fields = ['title', 'content']
 
 class SeriesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'create_date')
+    list_display = ('title', 'create_date', 'published_date')
     search_fields = ['title', 'content']
     fields = ['title', 'content']
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'create_date')
+    list_display = ('title', 'create_date', 'published_date')
     search_fields = ['title', 'content']
     fields = ['title', 'content']
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
-admin.site.register(Series)
-admin.site.register(Tag)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Tag, TagAdmin)
